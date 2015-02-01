b6db0f70196baea314ed791a6f73cbe9258fdbbc
Manuel Mastrofini <manuel.mastrofini@last-horizon.org>
2015-02-01 12:25:29 +0100

    code clean up and tuning

--- src/main/java/com/lh/mvcex/HomeController.java
+++ src/main/java/com/lh/mvcex/HomeController.java
@@ -6,12 +6,13 @@ import java.util.Locale;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -32,15 +33,34 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping(value = { "/custom" }, method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	@RequestMapping(value = { "/custom" }, method = RequestMethod.GET)
 	public @ResponseBody String home_custom(Locale locale, Model model) {
 
 		return "<html><body>CIAO</body></html>";
 	}
 
-	@RequestMapping(value = { "/static" }, method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	@RequestMapping(value = { "/static" }, method = RequestMethod.GET)
 	public @ResponseBody Resource home_static(Locale locale, Model model) {
 		Resource resource = new ClassPathResource("home.html");
 		return resource;
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+
+		return model;
+
 	}
 }
